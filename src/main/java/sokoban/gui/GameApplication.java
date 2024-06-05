10e825f85478741e993f9ab3fb7d91e39acd2ae3
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 15:24:36 +0300

    feat: add GameApplication to sokoban.gui package

---
+++ src/main/java/sokoban/gui/GameApplication.java
@@ -0,0 +1,43 @@
+package sokoban.gui;
+
+import com.gluonhq.ignite.guice.GuiceContext;
+import com.google.inject.AbstractModule;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.tinylog.Logger;
+import sokoban.results.GameResultRepository;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.io.IOException;
+import java.util.List;
+
+public final class GameApplication extends Application {
+    private final GuiceContext context = new GuiceContext(this, () -> List.of(
+            new AbstractModule() {
+                @Override
+                protected void configure() {
+                    bind(GameResultRepository.class).in(Singleton.class);
+                }
+            }
+    ));
+
+    @Inject
+    private FXMLLoader fxmlLoader;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Logger.info("Starting application");
+        context.init();
+        fxmlLoader.setLocation(getClass().getResource("/fxml/opening.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setTitle("Sokoban Game");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+}
