02b00417f8a04aacb490e57ca79c584ccdd7b44e
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 14:53:11 +0300

    feat: implement ResourcesRepository class for managing resources with JacksonJsonRepository

--- src/main/java/util/repository/ResourcesRepository.java
+++ src/main/java/util/repository/ResourcesRepository.java
@@ -1,4 +1,32 @@
 package util.repository;
 
+import lombok.NonNull;
+
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+
+
 public abstract class ResourcesRepository<T> extends JacksonJsonRepository {
+
+    private final Set<T> elements;
+
+    protected ResourcesRepository(
+            @NonNull final Class<T> elementClass,
+            @NonNull final String resourceName) {
+        try {
+            elements = Collections.unmodifiableSet(
+                    readSet(elementClass.getResourceAsStream(resourceName), elementClass)
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new AssertionError("Failed to load resource " + resourceName, e); // Can't happen
+        }
+    }
+
+    public Set<T> getAll() {
+        return elements;
+    }
 }
