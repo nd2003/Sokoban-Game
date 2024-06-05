9486067836cd30b78bf1d3841faeb6734c182262
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 15:49:33 +0300

    feat: implement FileChooserHelper methods for opening and saving JSON files

--- src/main/java/util/javafx/FileChooserHelper.java
+++ src/main/java/util/javafx/FileChooserHelper.java
@@ -1,4 +1,21 @@
 package util.javafx;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.Optional;
+
 public class FileChooserHelper {
+    public static Optional<File> show(boolean isOpen, Stage stage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("json documents", "*.json"));
+        if (isOpen) {
+            fileChooser.setTitle("Browse a JSON file to load the scoreboard");
+            return Optional.ofNullable(fileChooser.showOpenDialog(stage));
+        } else {
+            fileChooser.setTitle("Browse a JSON file to save the scoreboard");
+            return Optional.ofNullable(fileChooser.showSaveDialog(stage));
+        }
+    }
 }
