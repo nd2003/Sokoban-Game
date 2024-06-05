80ce118b3c74bba318e4886e0e88a3461a9298f6
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 16:00:28 +0300

    docs: added detailed comments to Stopwatch class

--- src/main/java/util/javafx/Stopwatch.java
+++ src/main/java/util/javafx/Stopwatch.java
@@ -11,6 +11,9 @@ import javafx.beans.property.StringProperty;
 import javafx.util.Duration;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 
+/**
+ * A general-purpose stopwatch.
+ */
 public class Stopwatch {
 
     private LongProperty seconds = new SimpleLongProperty();
@@ -18,6 +21,9 @@ public class Stopwatch {
 
     private Timeline timeline;
 
+    /**
+     * Creates a {@code Stopwatch} object.
+     */
     public Stopwatch() {
         timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> seconds.set(seconds.get() + 1)),
                 new KeyFrame(Duration.seconds(1)));
@@ -26,22 +32,39 @@ public class Stopwatch {
                 "HH:mm:ss"), seconds));
     }
 
+    /**
+     * {@return a property to access the number of seconds elapsed}
+     */
     public LongProperty secondsProperty() {
         return seconds;
     }
 
+    /**
+     * {@return a property to access the time elapsed in {@code hh:mm:ss} format}
+     */
     public StringProperty hhmmssProperty() {
         return hhmmss;
     }
 
+    /**
+     * Starts the stopwatch.
+     */
     public void start() {
         timeline.play();
     }
 
+    /**
+     * Pauses the stopwatch.
+     */
     public void stop() {
         timeline.pause();
     }
 
+    /**
+     * Resets the stopwatch. In order to reset the stopwatch it must be paused first with the {@link #stop()} method.
+     *
+     * @throws IllegalStateException if the stopwatch is not paused
+     */
     public void reset() {
         if (timeline.getStatus() != Animation.Status.PAUSED) {
             throw new IllegalStateException();
@@ -49,6 +72,9 @@ public class Stopwatch {
         seconds.set(0);
     }
 
+    /**
+     * {@return the status of the stopwatch}
+     */
     public Animation.Status getStatus() {
         return timeline.getStatus();
     }
