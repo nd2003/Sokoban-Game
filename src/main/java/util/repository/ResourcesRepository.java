b8d0fad027ed96692c0853aa4b872aef40a624cc
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 14:54:44 +0300

    docs: add class and method-level comments to ResourcesRepository class

--- src/main/java/util/repository/ResourcesRepository.java
+++ src/main/java/util/repository/ResourcesRepository.java
@@ -9,10 +9,26 @@ import java.util.Collections;
 import java.util.Set;
 
 
+/**
+ * Represents a repository to obtain a list of objects. Data is read from a
+ * static JSON file that contains an array of JSON objects. The file is loaded
+ * via the classloader.
+ *
+ * @param <T> the type of the elements
+ */
 public abstract class ResourcesRepository<T> extends JacksonJsonRepository {
 
     private final Set<T> elements;
 
+    /**
+     * Creates a {@code Repository} object to obtain a list of objects of
+     * {@code elementClass}. The classloader of {@code elementClass} is used to
+     * read JSON data.
+     *
+     * @param elementClass represents the type of the elements
+     * @param resourceName the name of the resource that contains JSON data to
+     *                     be read
+     */
     protected ResourcesRepository(
             @NonNull final Class<T> elementClass,
             @NonNull final String resourceName) {
@@ -26,6 +42,9 @@ public abstract class ResourcesRepository<T> extends JacksonJsonRepository {
         }
     }
 
+    /**
+     * {@return the set of objects}
+     */
     public Set<T> getAll() {
         return elements;
     }
