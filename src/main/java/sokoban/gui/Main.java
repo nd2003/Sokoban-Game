eb3f473a4fb359920bfffc0feeaf39899a61d70e
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 16:05:23 +0300

    feat: create Main class in sokoban.gui for launching Sokoban game

---
+++ src/main/java/sokoban/gui/Main.java
@@ -0,0 +1,10 @@
+package sokoban.gui;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(GameApplication.class, args);
+    }
+}
