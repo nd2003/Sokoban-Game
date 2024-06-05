a3fdc1ab0ec560cc73f7222d8f58cedcb4b793bd
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 14:47:18 +0300

    feat: implement utility methods for reading and writing sets using Jackson JSON

--- src/main/java/util/repository/JacksonJsonRepository.java
+++ src/main/java/util/repository/JacksonJsonRepository.java
@@ -1,4 +1,30 @@
 package util.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.NonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+
 public class JacksonJsonRepository {
+    protected static final ObjectMapper MAPPER = new ObjectMapper().registerModule(new JavaTimeModule());
+
+
+    protected static <T> Set<T> readSet(
+            @NonNull final InputStream in,
+            @NonNull final Class<T> elementClass) throws IOException {
+
+        final var type = MAPPER.getTypeFactory().constructCollectionType(Set.class, elementClass);
+        return MAPPER.readValue(in, type);
+    }
+
+    protected static <T> void writeSet(
+            @NonNull final OutputStream os,
+            @NonNull final Set<T> value) throws IOException {
+
+        MAPPER.writeValue(os, value);
+    }
 }
