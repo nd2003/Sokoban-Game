ad2fff5bd615eefb335770d924bb299f4b865af4
ndavid3 <ndavid21@mailbox.unideb.hu>
2024-06-05 13:32:16 +0300

    feat: Add board initialization and position bindings to SokobanState

--- src/main/java/sokoban/state/SokobanState.java
+++ src/main/java/sokoban/state/SokobanState.java
@@ -1,10 +1,71 @@
 package sokoban.state;
 
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import puzzle.State;
 
 import java.util.Set;
 
 public class SokobanState implements State<Direction> {
+
+    /**
+     * Instantiate the board.
+     */
+    public static Map board = new Map();
+
+    public static final int BOARD_SIZE = board.width();
+
+    public static final char WALL = '#';
+    public static final char TARGET = '.';
+    public static final char OUT_OF_BOARD = '-';
+
+    public static final int PLAYER_POSITION = 0;
+
+    public static final int FIRST_BOX_POSITION = 1;
+    public static final int SECOND_BOX_POSITION = 2;
+    public static final int THIRD_BOX_POSITION = 3;
+
+    private ReadOnlyObjectWrapper<Position>[] positions;
+
+    private final ReadOnlyBooleanWrapper solved;
+
+    public SokobanState() {
+        this(new Position(1, 1),
+                new Position(2, 2),
+                new Position(3, 2),
+                new Position(2, 3));
+        board = new Map();
+    }
+
+    public static final Position[] finishPositions =  {
+            new Position(3,7), new Position(4,7), new Position(5,7)
+    };
+
+    public SokobanState(Position... positions) {
+        checkPositions(positions);
+        this.positions = new ReadOnlyObjectWrapper[4];
+        for (var i = 0; i < 4; i++) {
+            this.positions[i] = new ReadOnlyObjectWrapper<>(positions[i]);
+        }
+        solved = new ReadOnlyBooleanWrapper();
+
+        solved.bind((this.positions[1].isEqualTo(finishPositions[0]).
+                or(this.positions[1].isEqualTo(finishPositions[1])).
+                or(this.positions[1].isEqualTo(finishPositions[2]))).
+
+                and(this.positions[2].isEqualTo(finishPositions[0]).
+                        or(this.positions[2].isEqualTo(finishPositions[1])).
+                        or(this.positions[2].isEqualTo(finishPositions[2]))).
+
+                and(this.positions[3].isEqualTo(finishPositions[0]).
+                        or(this.positions[3].isEqualTo(finishPositions[1])).
+                        or(this.positions[3].isEqualTo(finishPositions[2]))));
+    }
+
+    private void checkPositions(Position[] positions) {
+
+    }
+
     @Override
     public boolean isSolved() {
         return false;
